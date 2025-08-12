ce42384afcdf8dcdfdc1a25e9a35748b00e3a3a3
Javier Escandarani <javoescan@gmail.com>
2025-08-12 23:17:33 +0200

    WIP: Homepage

--- app/(frontend)/home/page.tsx
+++ app/(frontend)/home/page.tsx
@@ -9,6 +9,14 @@ export default function Home() {
 				width={'100%'}
 				style={{maxHeight: '300px' }}
 			/>
+			<div className="d-flex flex-column flex-lg-row justify-content-between p-5">
+				<div id="home-main-left-container" style={{backgroundColor: 'red'}}>
+					<div style={{height: 200, width: 650}}></div>
+				</div>
+				<div className="mt-3 mt-lg-0" id="home-main-right-container" style={{backgroundColor: 'black'}}>
+					<div style={{height: 500, width: 200}}></div>
+				</div>
+			</div>
 		</div>
 	);
 }
