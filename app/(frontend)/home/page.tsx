c5d165e33fa06ad7df2ab5ea1090c0bed01713c9
Javier Escandarani <javoescan@gmail.com>
2025-08-13 23:08:15 +0200

    Homepage finished

--- app/(frontend)/home/page.tsx
+++ app/(frontend)/home/page.tsx
@@ -1,30 +1,111 @@
-import { Image } from "react-bootstrap";
-import './page.css';
+import { Button, Image, Table } from "react-bootstrap";
+import "./page.css";
+import { event, onlineATC, onlineFlights, topControllers, topPilots } from "./temp_dataset";
 
 export default function Home() {
 	return (
-		<div className="">
+		<div className="home">
 			<Image 
-				src="/images/pages/home/banner.gif" 
+				src={event.image}
 				alt="banner"
 				width={'100%'}
 				style={{maxHeight: '300px' }}
 			/>
+			{/* LEFT CONTAINER */}
 			<div className="d-flex flex-column flex-lg-row justify-content-between p-5 pt-4">
 				<div className="home-left-container">
-					<div className="home-events">
+					<div className="home-us mb-4">
+						<p className="section-header">¿Qué es VATSIM Argentina?</p>
+						<div className="border-blue p-2">
+							<p>
+								VATSIM Argentina es la división de VATSIM encargada de gestionar el espacio aéreo virtual
+								argentino dentro de la red en donde podes participar como piloto volando o como controlador en algunas
+								de las dependencias del pais. Organizamos eventos masivos, realizamos entrenamiento tanto
+								para pilotos como para controladores. Es completamente gratuita, ¡unite a nuestra comunidad!
+							</p>
+							<Button className="btn-secondary w-100" href="/division/first-steps">Unirse</Button>
+						</div>
+					</div>
+					<div className="home-events mb-4">
 						<p className="section-header">Siguiente evento</p>
 						<div className="home-event">
 							<Image src="/images/events/event.jpeg" alt="event" className="home-event"/>
 						</div>
 					</div>
+					<div className="home-ranking">
+						<p className="section-header">Ranking</p>
+						<div className="home-ranking-data d-flex flex-column flex-lg-row">
+							<Table striped bordered hover className="me-5">
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Piloto</th>
+										<th>Tiempo</th>
+									</tr>
+								</thead>
+								<tbody>
+									{
+										topPilots.map((item, index) => 
+											<tr key={item.id}>
+												<td>{index + 1}</td>
+												<td>{item.name} - {item.id}</td>
+												<td>{item.hours}</td>
+											</tr>
+										)
+									}
+								</tbody>
+							</Table>
+							<Table striped bordered hover>
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Controlador</th>
+										<th>Tiempo</th>
+									</tr>
+								</thead>
+								<tbody>
+									{
+										topControllers.map((item, index) => 
+											<tr key={item.id}>
+												<td>{index + 1}</td>
+												<td>{item.name} - {item.id}</td>
+												<td>{item.hours}</td>
+											</tr>
+										)
+									}
+								</tbody>
+							</Table>
+						</div>
+					</div>
 				</div>
-				<div className="mt-3 mt-lg-0 ms-lg-5 home-right-container">
-					<p className="section-header">ATC Online</p>
-					<div className="home-online-atc-list">
-						<p className="home-online-atc-item">SAEF_CTR 134.500 - Javier Escandarani</p>
-						<p className="home-online-atc-item">SACF_CTR 128.800 - Giulano Viola</p>
-						<p className="home-online-atc-item">SAMF_CTR 126.600 - Mariano Lopez</p>
+
+				{/* RIGHT CONTAINER */}
+				<div className="mt-3 mt-lg-0 ms-lg-5 pb-5 home-right-container">
+					<div className="home-online-atc mb-4">
+						<p className="section-header">ATC Online</p>
+						<div className="home-online-list">
+							{
+								onlineATC.length ? onlineATC.map((item) => 
+									<div key={item.position} className="home-online-item border-blue">
+										<Image src={"/images/icons/connected.png"} alt="connected" height={10} width={10} /> 
+										<p className="home-online-item-text ms-2">{item.position} {item.frequency} - {item.controller}</p>
+									</div>
+								) : <div className="home-online-item border-blue">No hay controladores conectados</div>
+							}
+						</div>
+					</div>
+					<div className="home-online-flights">
+						<p className="section-header">Vuelos Online</p>
+						<div className="home-online-list">
+							{
+								onlineFlights.length ? onlineFlights.map((item) => 
+									<div key={item.callsign} className="home-online-item border-blue">
+										<Image src={"/images/icons/plane.png"} alt="radar" height={20} width={20} /> 
+										<p className="home-online-item-text ms-2">{item.callsign} {item.from}{"->"}{item.to} - {item.pilot}</p>
+									</div>
+								) : <div className="home-online-item border-blue">No hay pilotos conectados</div>
+							}
+						</div>
 					</div>
 				</div>
 			</div>
---
+++ app/(frontend)/home/temp_dataset.tsx
@@ -0,0 +1,197 @@
+
+const event = {
+	image: "/images/pages/home/banner.gif" 
+};
+
+const topPilots = [
+
+	{
+		name: "Javier Escandarani",
+		id: "133070",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "13022030",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "98680",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1330790",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1434790",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1307790",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1266590",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "7333260",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1305490",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1964390",
+		hours: "57.02"
+	},
+]
+
+const topControllers = [
+	{
+		name: "Javier Escandarani",
+		id: "1308070",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1308030",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1308080",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "13080790",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "13044790",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "13063490",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "1307490",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "7333360",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "13080740",
+		hours: "57.02"
+	},
+	{
+		name: "Javier Escandarani",
+		id: "13080730",
+		hours: "57.02"
+	},
+]
+
+const onlineATC = [
+	{
+		position: "SAEF_CTR",
+		frequency: "134.500",
+		controller: "Javier Escandarani"
+	},
+	{
+		position: "SACF_CTR",
+		frequency: "128.800",
+		controller: "Giuliano Viola"
+	},
+	{
+		position: "SAMF_CTR",
+		frequency: "126.600",
+		controller: "Mariano Lopez"
+	},,
+	{
+		position: "SAME_TWR",
+		frequency: "126.600",
+		controller: "Mariano Lopez"
+	},,
+	{
+		position: "SABE_GND",
+		frequency: "126.600",
+		controller: "Mariano Lopez"
+	},,
+	{
+		position: "SAZS_APP",
+		frequency: "126.600",
+		controller: "Mariano Lopez"
+	},
+];
+
+const onlineFlights = [
+	{
+		callsign: "ARG1680",
+		from: "SABE",
+		to: "SAZS",
+		pilot: "Javier Escandarani"
+	},
+	{
+		callsign: "JES3102",
+		from: "SAZN",
+		to: "SACO",
+		pilot: "Giuliano Viola"
+	},
+	{
+		callsign: "FBZ5407",
+		from: "SAWH",
+		to: "SAWC",
+		pilot: "Mariano Lopez"
+	},
+	{
+		callsign: "JES3103",
+		from: "SAZN",
+		to: "SACO",
+		pilot: "Giuliano Viola"
+	},
+	{
+		callsign: "JES3104",
+		from: "SAZN",
+		to: "SACO",
+		pilot: "Giuliano Viola"
+	},
+	{
+		callsign: "JES3106",
+		from: "SAZN",
+		to: "SACO",
+		pilot: "Giuliano Viola"
+	},
+	{
+		callsign: "JES3100",
+		from: "SAZN",
+		to: "SACO",
+		pilot: "Giuliano Viola"
+	},
+];
+
+export {
+	event,
+	topPilots,
+	topControllers,
+	onlineATC,
+	onlineFlights,
+}
